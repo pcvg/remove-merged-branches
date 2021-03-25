42302ab5f266f823dfd03e615a271502889c75c3
Alejandro Murcia <alejandro.murcia@savings-united.com>
2021-03-25 13:39:42 +0100

    Update README + entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 branches=`git branch -r --merged $GITHUB_ACTION_REF | grep -v "main$" | grep -v "master$" | grep -v "production$" | grep -v "staging$" | grep -v "$INPUT_PROTECTED_BRANCH$"`
 for branch in $branches; do
-    branch=`echo $branch | sed 's/remotes\/origin\///g'`
+    branch=`echo $branch | sed 's/origin\///g'`
     if [[ $branch != $GITHUB_ACTION_REF && $branch != $INPUT_PROTECTED_BRANCH ]]; then
         git push origin --delete $branch
         echo "$branch removed."
