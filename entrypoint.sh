ddabcf8845210d12b217976cdf4ce3f260e654f0
Alejandro Murcia <alejandro.murcia@savings-united.com>
2021-03-30 13:06:35 +0200

    fixes to identify the trigger branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
-
-branches=`git branch -r --merged $GITHUB_ACTION_REF | grep -v "main$" | grep -v "master$" | grep -v "production$" | grep -v "staging$" | grep -v "$INPUT_PROTECTED_BRANCH$"`
+GITHUB_BRANCH_ACTION=${INPUT_GITHUB_BRANCH_ACTION##*/}
+branches=`git branch -r --merged $GITHUB_BRANCH_ACTION | grep -v "main$" | grep -v "master$" | grep -v "production$" | grep -v "staging$" | grep -v "$INPUT_PROTECTED_BRANCH$"`
 for branch in $branches; do
-    branch=`echo $branch | sed 's/remotes\/origin\///g'`
-    if [[ $branch != $GITHUB_ACTION_REF && $branch != $INPUT_PROTECTED_BRANCH ]]; then
+    branch=`echo $branch | sed 's/origin\///g'`
+    if [[ $branch != $GITHUB_BRANCH_ACTION && $branch != $INPUT_PROTECTED_BRANCH ]]; then
         git push origin --delete $branch
         echo "$branch removed."
     else
