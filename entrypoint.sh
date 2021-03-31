a85875925838fe63a7a81997c7c8f07e5b725509
Alejandro Murcia <alejandro.murcia@savings-united.com>
2021-03-31 10:00:23 +0200

    change var name for trigger branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
-GITHUB_BRANCH_ACTION=${INPUT_GITHUB_BRANCH_ACTION##*/}
-branches=`git branch -r --merged $GITHUB_BRANCH_ACTION | grep -v "main$" | grep -v "master$" | grep -v "production$" | grep -v "staging$" | grep -v "$INPUT_PROTECTED_BRANCH$"`
+HEAD_BRANCH=${INPUT_HEAD_BRANCH##*/}
+branches=`git branch -r --merged $HEAD_BRANCH | grep -v "main$" | grep -v "master$" | grep -v "production$" | grep -v "staging$" | grep -v "$INPUT_PROTECTED_BRANCH$"`
 for branch in $branches; do
     branch=`echo $branch | sed 's/origin\///g'`
-    if [[ $branch != $GITHUB_BRANCH_ACTION && $branch != $INPUT_PROTECTED_BRANCH ]]; then
+    if [[ $branch != $HEAD_BRANCH && $branch != $INPUT_PROTECTED_BRANCH ]]; then
         git push origin --delete $branch
         echo "$branch removed."
     else
